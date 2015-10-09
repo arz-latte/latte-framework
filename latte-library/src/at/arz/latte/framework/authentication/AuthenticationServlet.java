d9626d70fff1845ee7af1a2873a85395223ea1bc
mrodler <mrodler@users.noreply.github.com>
2015-10-09 19:05:10 +0200

    latte-library zu web-fragment umgebaut. AuthenticationServlet und StaticPageFilter in web-fragment verschoben

--- latte-library/src/at/arz/latte/framework/authentication/AuthenticationServlet.java
+++ latte-library/src/at/arz/latte/framework/authentication/AuthenticationServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AuthenticationServlet extends HttpServlet {
-
 	private static final long serialVersionUID = 1L;
 
 	private static final String REDIRECT_COOKIE_NAME = "_redirect_dst";
